746f47478ed754fd839b0c76eb5d4deaca6a20ea
Cassio Moreto <kassyuz@gmail.com>
2015-11-17 01:27:38 -0200

    Primeiro projeto - WebView acessando o site com o mapa e pegando a localizacao do usuario.

---
+++ app/src/main/java/com/mobile/foodparking/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mobile.foodparking;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("http://foodparking.com");
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
