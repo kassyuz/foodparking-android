e38a253d4143ef6073025d6271a14901e34c379f
Cassio Moreto <cassio.moreto@fluig.com>
2015-11-18 19:52:23 -0200

    Criado botao de reload apos voltar conexao de dados.

--- app/src/main/java/com/mobile/foodparking/MainActivity.java
+++ app/src/main/java/com/mobile/foodparking/MainActivity.java
@@ -2,18 +2,23 @@ package com.mobile.foodparking;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 
     private WebView mWebView;
+    private Button refreshButton;
 
 
     /**
@@ -49,10 +54,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        refreshButton = (Button) findViewById(R.id.refreshButton);
 
         // Brower niceties -- pinch / zoom, follow links in place
         mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
         mWebView.setWebViewClient(new GeoWebViewClient());
 
         // Below required for geolocation
@@ -62,11 +69,27 @@ public class MainActivity extends Activity {
 
         //load URL
         if(isOnline()){
-            mWebView.loadUrl("http://foodparking.com");
+            withNetworkConnection();
         } else{
-            mWebView.loadData("Sem conexão", "text/plain", "UTF8");
-
+            withoutNetworkConnection();
         }
+    }
+
+    private void withNetworkConnection() {
+        ((ViewGroup) refreshButton.getParent()).removeView(refreshButton);
+        mWebView.loadUrl("http://foodparking.com");
+    }
+
+    private void withoutNetworkConnection(){
+        mWebView.loadData("Sem conexão", "text/plain", "UTF8");
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               withNetworkConnection();
+            }
+        });
+
     }
 
     @Override
@@ -83,5 +106,11 @@ public class MainActivity extends Activity {
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
         return (networkInfo != null && networkInfo.isConnectedOrConnecting());
+    }
+
+    private void restartApp(){
+        Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 }
